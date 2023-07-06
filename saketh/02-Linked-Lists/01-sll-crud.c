2cde9580c149b3993e32de6577df5a05d161301a
bhagavan <bhagavan@auranetworks.in>
2023-07-06 12:51:44 +0530

    Suggesting fixes

--- saketh/02-Linked-Lists/01-sll-crud.c
+++ saketh/02-Linked-Lists/01-sll-crud.c
@@ -1,51 +1,67 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 struct student
 {
 	int v;
 	struct student *next;
 };
-struct student *h;
-int c(int n)
+
+struct student *H;
+
+int add_node(int n)
 { 
 	struct student *t;
 	struct student *new=malloc(sizeof(struct student));
-	new->v=n;
+	new->v = n;
 	new->next = NULL;
-	 if(h==NULL){
-		 h=new;
-		 return 0;
-	 }
-    for( t=h;t->next!=NULL;t=t->next);
-	t->next=new;
-	 return 0;
+	if(H==NULL)
+	{
+		H=new;
+		return 0;
+	}
+
+	for(t = H; t->next != NULL; t = t->next);
+	t->next = new;
+	return 0;
 }
-void r(void)
+
+void dump_list(void)
 {
-	struct student *t=h;
-	for(t=h;t->next!=NULL;t=t->next)
-		printf("\n %d",t->v);
-	printf("\n %d \n",t->v);
+	struct student *t=H;
+	for(t=H;t->next!=NULL;t=t->next)
+		printf("%3d",t->v);
+	
+	//TODO: you can print last node value inside the loop
+	printf("%3d",t->v);
+	printf("\n");
 }
-void u(int ov,int nv)
+
+void update_node_by_value(int ov,int nv)
 {
-	struct student *t=h;
-	for(t=h;t->next!=NULL && t->v!=ov ;t=t->next);
+	struct student *t=H;
+	//TODO: What happens if the 'ov' value doesn't exists?
+	for(t=H;t->next!=NULL && t->v!=ov ;t=t->next);
 	t->v=nv;
 }
-int d(int val)
+
+int delete_node_by_value(int val)
 {
-	struct student *t=h;
-	for(t=h; t->next->v!= val ; t=t->next);
+	//TODO: What about below deleation cases?
+	//First,
+	//Last,
+	//Single Node,
+	//No Nodes cases?
+	struct student *t=H;
+	for(t=H; t->next->v!= val ; t=t->next);
 	t->next=t->next->next;
 	return 0;
 }
+
 int main()
 {	
-	c(5);
-	c(6);
-	c(8);
-	r();
-	d(6);
-	r();
+	add_node(5);
+	add_node(6);
+	add_node(8);
+	dump_list();
 }	
