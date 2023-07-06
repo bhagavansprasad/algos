f762099e16727863583c3e89edda220b657f997e
bhagavan <bhagavan@auranetworks.in>
2023-07-06 01:16:31 -0400

    adding modified files

--- saketh/01-Arrays/bubble_sort.c
+++ saketh/01-Arrays/bubble_sort.c
@@ -1,36 +1,33 @@
+
 #include <stdio.h>
-void main(void)
+
+void bubble(int a[])
 {
-    int i=0, j=i+1,t=0,k,v,n=5;
-    int a[4] = {5, 9, 1, 3};
-	printf("\n before sorting \n");
-    for (k = 0; k <= n - 1; k++)
-		printf(" %d  ",k);
-	printf("\n");
-	n=n-1;
-	for (v = 0; v <= n - 1; v++)
-		printf(" %d  ",a[v]);
-	for(i=0;i<=4;i++,n--)
-	{	
-		for (j=0; j <= n - 1;j++)
-		{
-			if(a[j]>a[j+1])
-			{	
-				t=a[j];
-				a[j]=a[j+1];
-				a[j+1]=t;
-			}	
-		}
-		printf("\n");
-	}
-	n=4;
-    printf("\n \n\n\n after sorting \n");
-    for (k = 0; k <= n - 1; k++)
-		printf(" %d  ",k);
-	printf("\n");
-    for (v = 0; v <= n - 1; v++)
-		printf(" %d  ",a[v]);
-	printf("\n");
-	printf("\n");
-	
-}	
+    int i,j,t,k,v,n=5;
+    for (v = 0;v<= n-1;v++)
+        printf(" %d  ",a[v]);
+    for (i=0;i<4;i++,n--)
+    {    
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {    
+                t = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = t;
+            }    
+        }
+        printf("\n");
+    }
+    n = 5;
+    printf("\n\n\nAfter sorting:\n");
+    for (k = 0; k < n; k++)
+        printf(" %d  ", a[k]);
+    printf("\n");
+    printf("\n");
+}
+void main()
+{
+    int a[] = {5, 9, 1, 3, 7};
+    bubble(a);
+}
